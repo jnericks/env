ddf154162fab740408f8010d694abd91809db9ce
Jon Erickson <erjonath@twitch.tv>
2022-11-09 09:38:08 -0800

    update brewfile, config, some alfred workflows

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,10 @@
 # ln -s $ENV_DIR/fish $HOME/.config/fish
 
 fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
 
 set -gx ENV_DIR $HOME/env
+set -gx EDITOR  code
 
  # load configs
 for cfg in (ls $ENV_DIR/**/*config.fish | grep -v /fish/config.fish)
@@ -14,6 +16,8 @@ end
 if test -f $ENV_DIR/fish/twitch.fish
     source $ENV_DIR/fish/twitch.fish
 end
+
+alias df '$EDITOR $ENV_DIR'
 
 # Navigation
 alias ..     'cd ..'
@@ -29,3 +33,9 @@ set -g theme_display_git_default_branch yes
 set -g theme_nerd_fonts yes
 set -g theme_newline_cursor yes
 set -g fish_prompt_pwd_dir_length 0
+
+function fish_user_key_bindings
+    # Please bind peco_select_history to your favorite key.
+    # If you'd like to bind Ctrl+r:
+    bind \cr 'peco_select_history (commandline -b)'
+end
